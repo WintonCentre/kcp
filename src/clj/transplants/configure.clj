7ced0ea540c74ff994850982613c5ad3c1369c04
gmp26 <gmp26@cam.ac.uk>
2020-04-21 23:57:14 +0100

    config.edn complete. get-rows and get-cols working

--- src/clj/transplants/configure.clj
+++ src/clj/transplants/configure.clj
@@ -3,10 +3,13 @@
    [aero.core :as aero]
    [dk.ative.docjure.spreadsheet :as xls]
    [clojure.java [io :as io]]
+   [clojure.string :as string]
    ))
 
 
-(defn read-config [path profile]
+(defn read-config
+  "read configuration file from the classpath using aero"
+  [path profile]
   (aero/read-config (io/resource path) {:profile profile}))
 
 (defn read-table
@@ -18,14 +21,15 @@
 
   Returns a collection of maps containing meaningful keys mapped to data.
   "
-  ([{:keys [path sheet columns header-rows]}]
-   (let [table (->> (xls/load-workbook path)
-                    (xls/select-sheet (second sheet))
-                    (xls/select-columns columns)
-                    (drop header-rows)
-                    )]
-     table
-     )))
+  [{:keys [path sheet columns header-rows]}]
+  (->> (xls/load-workbook path)
+       (xls/select-sheet (second sheet))
+       (xls/select-columns columns)
+       (drop header-rows)))
+
+(->> (xls/load-workbook "data/kidney-models-master.xlsx")
+     (xls/select-sheet "waiting-inputs"))
+
 ;;;
 ; spreadsheet configuration
 ;;;
@@ -34,35 +38,138 @@
   [profile]
   (aero/read-config "data/config.edn" {:profile profile}))
 
+(def memo-config (memoize get-config))
+
 ;;;
 ; switch organ here for now
 (def organ :kidney)
 ;(def organ :lung)
 ;;;
 
-(defn get-workbooks
+(defn get-workbook
   "Read in workbooks for an organ"
   [organ]
-  (into {}
-        (->> (:workbooks (get-config organ))
-             (map (fn [[k v]]
-                    (when v
-                      (println "loading " k v)
-                      [k (xls/load-workbook v)])))
-             )))
+  (xls/load-workbook (:workbook (memo-config organ)))
+  )
+
+(def memo-workbook
+  "memoize get-workbook because it reads in a load of data"
+  (memoize get-workbook))
+
+(defn get-sheet-spec
+  "get sheet spec given sheet-key"
+  [organ sheet-key]
+  (if-let [sheet (get-in (memo-config organ) [:sheets sheet-key])]
+    sheet
+    (throw (ex-info "Unable to read sheet"
+                    {:cause "sheet name missing?"
+                     :organ organ
+                     :sheet sheet-key}))))
+
+(defn get-header
+  "retrieve header variables in a seq"
+  [organ sheet-key]
+  (let [workbook (memo-workbook organ)
+        sheet (get-sheet-spec organ sheet-key)]
+    (->> (xls/select-sheet (name sheet-key) workbook)
+         (xls/row-seq)
+         (remove nil?)
+         (first)
+         (xls/cell-seq)
+         (map xls/read-cell)
+         (take-while #(and (some? %) (string/starts-with? % ":")))
+         (map #(subs % 1))
+         (map keyword)
+         )))
+
+(defn get-rows
+  "retrieve a sequence of maps - one map per row of data, with variables as keys"
+  ([organ sheet-key centre]
+   (->> (get-rows organ sheet-key)
+        (filter #(= (first %) centre))))
+  
+  ([organ sheet-key]
+   (let [workbook (memo-workbook organ)
+         sheet (get-sheet-spec organ sheet-key)
+         headers (get-header organ sheet-key)]
+     (->> (xls/select-sheet (name sheet-key) workbook)
+          (xls/row-seq)
+          (remove nil?)
+          (drop 1)
+          (map #(map xls/read-cell (take (count headers) %)))
+          ))))
+
+(defn row->map
+  [headers row]
+  (zipmap headers row))
+
+(defn transpose
+  "transpose rows of a matrix. If 2 arity, add headers"
+  ([m]
+   (apply map vector m))
+  ([m headers]
+   (zipmap headers (transpose m))))
+
+(defn get-columns
+  "retrieve a table as a map of vectors"
+  ([organ sheet-key centre]
+   (transpose
+    (get-rows organ sheet-key centre)
+    (get-header organ sheet-key)))
+  ([organ sheet-key]
+      (transpose
+       (get-rows organ sheet-key)
+       (get-header organ sheet-key))))
+#_(defn get-columns
+  "retrieve a table as a map of vectors"
+  ([organ sheet-key centre]
+   (->> (get-rows organ sheet-key centre)
+        (apply map vector)
+        (zipmap (get-header organ sheet-key))))
+  ([organ sheet-key]
+   (->> (get-rows organ sheet-key)
+        (apply map vector)
+        (zipmap (get-header organ sheet-key))))
+  )
 
 (comment
   (require '[clojure.pprint :refer [pp pprint]])
-  
-  ;; raw loading test
-  (xls/load-workbook "data/current-kidney/Competing risks CIF kidney.xlsx")
-  (xls/load-workbook "data/current-kidney/post tx patient and graft survival.xlsx")
+
+  (get-config :kidney)
+  (memo-config :kidney)
+  (get-config :lung)
 
   ;; configured loading test
-  (get-workbooks :kidney)
-  (get-workbooks :lung)
+  (get-workbook :kidney)
+  (get-workbook :lung)
+  (memo-workbook :kidney)
+
+  (get-sheet-spec :kidney :waiting-baseline-cifs)
+  (get-sheet-spec :kidney :waiting-baseline-vars)
+  (get-sheet-spec :lung :waiting-baseline-cifs)
+  (try (get-sheet-spec :kidney :foo)
+       (catch Exception e (ex-data e)))
+
+  (get-header :kidney :waiting-baseline-cifs)
+  (get-rows :kidney :waiting-baseline-cifs)
+  (get-rows :kidney :waiting-baseline-cifs "Newcastle")
+  (get-columns :kidney :waiting-baseline-cifs)
+  (get-columns :kidney :waiting-baseline-cifs "Newcastle")
+
   
-  (pprint (read-table  ((:tables (aero/read-config kidney-config)) 0)))
+  (get-header :kidney :waiting-baseline-vars)
+  (get-header :kidney :waiting-inputs)
+  (get-header :kidney :graft-baseline-cifs)
+  (get-header :kidney :graft-baseline-vars)
+  (get-header :kidney :graft-baseline-inputs)
+  (get-header :kidney :bmi-calculator)
+  (get-header :kidney :survival-baseline-cifs)
+  (get-header :kidney :survival-baseline-vars)
+  (get-header :kidney :survival-inputs)
+
+  (get-header :lung :waiting-baseline-cifs)
+
+
+  (pprint (read-table  ((:tables (aero/read-config :kidney)) 0)))
   (pp)
-  (pprint *1)
-  )
+  (pprint *1))
---
+++ upload-xlsx.clj
@@ -0,0 +1,37 @@
+#!/usr/bin/env bb
+;
+; To attach nrepl in Calva for babashka debug:
+; 1) In a terminal run `bb --nrepl-server 1667`
+; 2) Use ctl-alt-C ctl-alt-C and jack-in to "General"
+; 3) Connect to localhost:1667 on the next prompt.
+;
+(require '[clojure.java.shell :refer [sh]]
+         '[cheshire.core :as :json]
+         )
+
+(def home (.getProperty System "user.home"))
+(def slash File/separator)
+
+;
+; Locate Dropbox path through ~/.dropbox/info.json 
+; On my system it's under the "business" key
+;
+(def db-info (str home
+                  slash ".dropbox"
+                  slash "info.json"))
+(def db-map (json/parse-string (slurp db-info)))
+(def db-path (get-in db-map ["business" "path"]))
+(def transplant (str db-path
+                     slash "Winton Centre" 
+                     slash "TRANSPLANT"
+                     slash "configuration"))
+(def kidney (str "data" slash "kidney-models-master.xlsx"))
+(def lung (str "data" slash "lung-models-master.xlsx"))
+
+(comment
+  (sh "ls" lung)
+  (sh "ls" kidney)
+  (sh "ls" transplant))
+
+; publish the xlsx on dropbox
+(:exit (sh "cp" lung kidney transplant))

1b0fdb837d061c4d0ecc818a5328621f15843c6f
gmp26 <gmp26@cam.ac.uk>
2020-05-04 11:15:22 +0100

    fix typo, (and allow deftest for validation)

--- src/cljs/transplants/bsio.cljs
+++ src/cljs/transplants/bsio.cljs
@@ -1,9 +1,7 @@
 (ns transplants.bsio
   "A (react) bootstrap i/o wrapper. There's an example of a boostrap text input component in the comment
 where we can work on defining a common interface. 
-
-We're relaxing the constraint of making the reagent components themselves unit-testable as that is simply
-too much work for too little benefit. Instead, test using devcards or etaoin."
+"
   ["react-bootstrap" :as bs])
 
 (comment
@@ -22,7 +20,7 @@ The caller can define `value-f` in a number of ways:
 (fn [] @ref-value)              ; Caller has already subscribed, but wants the component to deref.
 (fn [] plain-value)             ; Caller is simply providing a value. 
 By passing in a function the caller can control where and how often the dereferencing happens. 
-In all cases the bsio compononent simply calls value-f to get the needed value.
+In all cases the bsio component simply calls value-f to get the needed value.
 We can't assume that value-k identifies (value-f) though it often will.
 
 `event-f` is a function for reporting events that change the value referenced by value-k
@@ -38,7 +36,7 @@ The caller would have to provide wrapping Form components like InputGroup in tha
 I doubt that those would need to be part of this library. Each component would likely be wrapped in 
 its own Form in that case. That's for the next level up to worry about.
 
-Some experimentation with Forms is necessary to checki it's a viable approach that allows the 
+Some experimentation with Forms is necessary to check it's a viable approach that allows the 
 low level inputs to react to value changes without the user having to press a submit button. Hopefully it is.
 
 I've also missed out things like stopPropagation, preventDefault, and touch events.

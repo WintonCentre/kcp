efb3c90bd06cca6e28b5692a1e3d7b7a64c13d31
Jin Park <jinpark.han@gmail.com>
2020-05-19 12:37:12 +0100

    Add git hooks and script

---
+++ build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo "Please add appropriate build"
--- dev/cljs/hooks/pre-push.sh
+++ dev/cljs/hooks/pre-push.sh
@@ -5,21 +5,15 @@ do
     branch=$(git rev-parse --symbolic --abbrev-ref $refname)
 
 	if [ "staging/lung" == "$branch" ]; then
-#	    ./build.sh
-#        git add .
-#        git commit -m "Auto commit build output"
 		echo "staging/lung. Sending jenkins build."
-        curl -u jin:11da9bd347a5cd21b349ecab65fac5e31a https://jenkins.wintoncentre.uk/job/predict-breast-develop/build?token=raiej345bgiojgahu4
+        curl -u jin:11da9bd347a5cd21b349ecab65fac5e31a https://jenkins.wintoncentre.uk/view/NHS-BT/job/nhsbt-lung-staging/build?token=jgfanw4jwiroegj09234t93gaoiwe
 
         command -v git-lfs >/dev/null 2>&1 || { echo >&2 "\nThis repository is configured for Git LFS but 'git-lfs' was not found on your path. If you no longer wish to use Git LFS, remove this hook by deleting .git/hooks/pre-push.\n"; exit 2; }
         git lfs pre-push "$@"
 
 	elif [ "staging/kidney" == "$branch" ]; then
-#	    ./build.sh
-#        git add .
-#        git commit -m "Auto commit build output"
 		echo "staging/kidney. Sending jenkins build."
-        curl -u jin:11da9bd347a5cd21b349ecab65fac5e31a https://jenkins.wintoncentre.uk/job/predict-breast-staging/build?token=qweqweasd
+        curl -u jin:11da9bd347a5cd21b349ecab65fac5e31a https://jenkins.wintoncentre.uk/view/NHS-BT/job/nhsbt-kidney-staging/build?token=jgfanw4jwiroegj09234t93gaoiwe
 
         command -v git-lfs >/dev/null 2>&1 || { echo >&2 "\nThis repository is configured for Git LFS but 'git-lfs' was not found on your path. If you no longer wish to use Git LFS, remove this hook by deleting .git/hooks/pre-push.\n"; exit 2; }
         git lfs pre-push "$@"

9ebb594ffd93b87f46c175ec815e30b269ef5c46
Jin Park <jinpark.han@gmail.com>
2020-05-27 10:08:33 +0100

    Add develop/staging

---
+++ publish-ci-develop.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+echo "Publishing to development server"
+TARGET='transplants-dev.wintoncentre.uk:/var/www/transplants-dev.wintoncentre.uk/'
+LOCAL_DEV='resources/public/'
+echo "===============rsync-ing files==================="
+rsync -avz --omit-dir-times --del --no-perms --no-owner --no-group $LOCAL_DEV $USER@$TARGET
+echo "================================="
+echo "Uploaded to " $TARGET
+echo "================================="

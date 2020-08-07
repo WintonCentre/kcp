0921932261f772c34edb51712956cf67a2d7bcd8
gmp26 <gmp26@cam.ac.uk>
2020-08-07 18:16:14 +0100

    increased lein heap, added configure deps build

--- publish-ci-develop.sh
+++ publish-ci-develop.sh
@@ -4,7 +4,7 @@ set -e
 echo "Publishing to development server"
 TARGET='transplants-dev.wintoncentre.uk:/var/www/transplants-dev.wintoncentre.uk/'
 echo "generating configuration data"
-#lein config
+lein config
 LOCAL_DEV='resources/public/'
 echo "===============rsync-ing files==================="
 rsync -avz --omit-dir-times --del --no-perms --no-owner --no-group $LOCAL_DEV $USER@$TARGET

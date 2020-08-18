d1ae02e985f81e49968921896e237e07c290b247
gmp26 <gmp26@cam.ac.uk>
2020-08-18 13:16:38 +0100

    dev and prod ok, test not ok

---
+++ test/cljs/transplants/test_runner.cljs
@@ -0,0 +1,10 @@
+;; This test runner is intended to be run from the command line
+(ns transplants.test-runner
+  (:require
+    ;; require all the namespaces that you want to test
+    [transplants.bsio-test]
+    #_[transplants.core-test]
+    [figwheel.main.testing :refer [run-tests-async]]))
+
+(defn -main [& args]
+  (run-tests-async 5000))

943eee8af0144152ced2f775201bfbee5b9185ec
gmp26 <gmp26@cam.ac.uk>
2020-08-28 12:48:29 +0100

    saved db init for guidance-percent

--- src/cljs/transplants/db.cljs
+++ src/cljs/transplants/db.cljs
@@ -3,5 +3,6 @@
 (def default-db
   {:window-width 800 #_(.innerWidth js/Window)
    :background-info :visits
+   :guidance-percent 22
   ; Move these to the spreadsheet
    :cohort-dates {:from-year 2004 :to-year 2014}})

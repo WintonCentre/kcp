388677fa4657e468027f5ad646e2da782bb17afe
gmp26 <gmp26@cam.ac.uk>
2021-05-10 22:10:06 +0100

    remove nil description centres from menu

--- src/transplants/views.cljs
+++ src/transplants/views.cljs
@@ -51,6 +51,7 @@
        (if-not @tools
          [:div "loading " organ "/edn/tools.txt"]
          (let [centres (sort-by :description ((keyword organ) @centres))
+               centres (filter #(some? (:description %)) centres)
                tools @tools
                centre-card (fn [centre]
                              [ui/centre-card mobile

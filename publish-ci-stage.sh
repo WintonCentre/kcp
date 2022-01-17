608ce4a8297bb98a9c404e1351386eb9878e46a9
gmp26 <gmp26@cam.ac.uk>
2022-01-17 06:37:30 +0000

    public-lung -> public in publish-ci-stage

--- publish-ci-stage.sh
+++ publish-ci-stage.sh
@@ -3,7 +3,7 @@
 set -e
 echo "Publishing to staging server"
 TARGET='transplants-staging.wintoncentre.uk:/var/www/transplants-staging.wintoncentre.uk/'
-LOCAL_DEV='resources/public-lung/'
+LOCAL_DEV='resources/public/'
 echo "===============rsync-ing files==================="
 rsync -avz --omit-dir-times --del --no-perms --no-owner --no-group $LOCAL_DEV $USER@$TARGET
 echo "================================="

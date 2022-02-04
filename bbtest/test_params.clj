150cf63c880e794a2ea2d4326cf20ee62c02a361
gmp26 <gmp26@cam.ac.uk>
2022-02-04 09:14:45 +0000

    WIP on lung parameter tests

--- bbtest/test_params.clj
+++ bbtest/test_params.clj
@@ -4,21 +4,22 @@
    [clojure.string :as str]
    [prepare-test-data :as s]))
 
+(def organ "lung")
 (def organ-key (s/get-organ))
 
-(def test-options [{:organ "kidney", :tool "waiting", :centre "Belfast"}
-                   {:organ "kidney", :tool "survival", :centre "Belfast"}
-                   {:organ "kidney", :tool "ldsurvival", :centre "UK"}
-                   {:organ "kidney", :tool "graft", :centre "Belfast"}
-                   {:organ "kidney", :tool "ldgraft", :centre "UK"}
-                   #_{:organ "lung", :tool "waiting", :centre "Newcastle"}
-                   #_{:organ "lung", :tool "post-transplant", :centre "Newcastle"}])
+(def test-options [#_{:organ "kidney", :tool "waiting", :centre "Belfast"}
+                   #_{:organ "kidney", :tool "survival", :centre "Belfast"}
+                   #_{:organ "kidney", :tool "ldsurvival", :centre "UK"}
+                   #_{:organ "kidney", :tool "graft", :centre "Belfast"}
+                   #_{:organ "kidney", :tool "ldgraft", :centre "UK"}
+                   {:organ "lung", :tool "waiting", :centre "Newcastle"}
+                   {:organ "lung", :tool "post-transplant", :centre "Newcastle"}])
 (def outcomes {:kidney [[:beta-transplant "tx"]
                         [:beta-death "dth"]
                         [:beta-removal "rem"]]
-               #_#_:lung {:beta-translant "tx"
-                          :beta-death "rem"
-                          #_#_:beta-removal "rem"}})
+               :lung [[:beta-translant "tx"]
+                      [:beta-death "rem"]]
+               #_#_:beta-removal "rem"})
 
 (def rnfs (s/r-named-factors (first test-options)))
 
@@ -41,8 +42,6 @@
 (defn values-match
   [rnf r-outcome edn-outcome]
   (s/near? (get-r-value rnf r-outcome) (get-edn-value rnf edn-outcome)))
-
-(def organ "kidney")
 
 (deftest all-parameters
   (doseq [options test-options]
@@ -69,7 +68,23 @@
       #_(is map? options)))  ;;; placeholder
 
 
+(deftest test-all-lung-tx
+    (doseq [rnf (s/r-named-factors (first test-options))]
+      (testing (pr-str (get rnf :factor) (get rnf :level) "tx")
+        (let [a (get-r-value rnf "tx")
+              b (get-edn-value rnf :beta-transplant)]
+          (when (and a b)
+            (is (s/near? a b) (str a " and " b " differ")))))))
+(comment
+  (def rnfs (s/r-named-factors (first test-options)))
+  (def rnf (nth rnfs 1))
+  (get rnf :factor)
+  (get rnf :level)
+  (get-r-value rnf "tx")
 
+  (test-all-lung-tx)
+  )
+  
 #_(deftest test-all-kidney-tx
   (doseq [rnf (s/r-named-factors (first test-options))]
     (testing (pr-str (get rnf :factor) (get rnf :level) "tx")

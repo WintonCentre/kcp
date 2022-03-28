349e33d892c98c33a79d133d91c9db31288da7cc
gmp26 <gmp26@cam.ac.uk>
2022-03-28 10:54:32 +0100

    delete overflow-x wrap

--- src/transplants/widgets.cljs
+++ src/transplants/widgets.cljs
@@ -88,7 +88,6 @@
        [:b "enable the option which prints background graphics."]]
       [:h1 "QRCode"]
       [:p#docuri {:style {:width 466
-                          :overflow-x "wrap"
                           :word-break "break-word"}} URI]
       [:div#qrcode {:style {:display "flex" :flex-direction "column" :justify-content "top" :align-items "center" :margin-bottom 10}}]
       [:p "Use a QRCode reader on your mobile to view your inputs there."]

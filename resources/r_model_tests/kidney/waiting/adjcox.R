2b9302f5c00f906a9ae31b01113ffb1e82d68f93
gmp26 <gmp26@cam.ac.uk>
2022-03-31 21:40:12 +0100

    closing on kidney waiting test formats

--- resources/r_model_tests/kidney/waiting/adjcox.R
+++ resources/r_model_tests/kidney/waiting/adjcox.R
@@ -1,53 +1,56 @@
 ## Adjusted capH will be - log(tx_surv) * exp(XB) ##
 survdata <- read.csv("surv.csv")
 param <- read.csv("params.csv")
+tests <- read.csv("tests.csv")
 attach(survdata)
 attach(param)
+attach(tests)
 
-
-adjcox <- function(cent = "Belfast", sex = 1, rethnic_grp=1, diabetes=2, hsp_reg=1, rbg_new=1, dial_reg=1, matchpts_grp = 1, reg_age10 = 4, graft_no_new = 0) {
+# NB: we rationalise the adjcox formal parameter names to correspond better with the names used in params.csv.
+# This makes it easier to call adjcox automatically.
+adjcox <- function(cent = "Belfast", sex = "m", eth="white", diabetes=2, sens=1, bld="o", dialysis=1, match = 1, age = 4, graft_no_new = 0) {
   
   ## Calculate individual xbeta terms ##
   
   #Sex #
-  if(sex == 1){tx_xbeta_sex = tx_sex_m ; rem_xbeta_sex = rem_sex_m ; dth_xbeta_sex = dth_sex_m}
-  if(sex == 2){tx_xbeta_sex = tx_sex_f ; rem_xbeta_sex = rem_sex_f ; dth_xbeta_sex = dth_sex_f}
+  if(sex == "m"){tx_xbeta_sex = tx_sex_m ; rem_xbeta_sex = rem_sex_m ; dth_xbeta_sex = dth_sex_m}
+  if(sex == "f"){tx_xbeta_sex = tx_sex_f ; rem_xbeta_sex = rem_sex_f ; dth_xbeta_sex = dth_sex_f}
   
   #Age #
-  if(reg_age10 == 2){tx_xbeta_age = tx_age_2 ; rem_xbeta_age = rem_age_2 ; dth_xbeta_age = dth_age_2}
-  if(reg_age10 == 3){tx_xbeta_age = tx_age_3 ; rem_xbeta_age = rem_age_3 ; dth_xbeta_age = dth_age_3}
-  if(reg_age10 == 4){tx_xbeta_age = tx_age_4 ; rem_xbeta_age = rem_age_4 ; dth_xbeta_age = dth_age_4}
-  if(reg_age10 == 5){tx_xbeta_age = tx_age_5 ; rem_xbeta_age = rem_age_5 ; dth_xbeta_age = dth_age_5}
-  if(reg_age10 == 6){tx_xbeta_age = tx_age_6 ; rem_xbeta_age = rem_age_6 ; dth_xbeta_age = dth_age_6}
-  if(reg_age10 == 7){tx_xbeta_age = tx_age_7 ; rem_xbeta_age = rem_age_7 ; dth_xbeta_age = dth_age_7}
+  if(age == 2){tx_xbeta_age = tx_age_2 ; rem_xbeta_age = rem_age_2 ; dth_xbeta_age = dth_age_2}
+  if(age == 3){tx_xbeta_age = tx_age_3 ; rem_xbeta_age = rem_age_3 ; dth_xbeta_age = dth_age_3}
+  if(age == 4){tx_xbeta_age = tx_age_4 ; rem_xbeta_age = rem_age_4 ; dth_xbeta_age = dth_age_4}
+  if(age == 5){tx_xbeta_age = tx_age_5 ; rem_xbeta_age = rem_age_5 ; dth_xbeta_age = dth_age_5}
+  if(age == 6){tx_xbeta_age = tx_age_6 ; rem_xbeta_age = rem_age_6 ; dth_xbeta_age = dth_age_6}
+  if(age == 7){tx_xbeta_age = tx_age_7 ; rem_xbeta_age = rem_age_7 ; dth_xbeta_age = dth_age_7}
   
   #Ethnicity #
-  if(rethnic_grp == 1){tx_xbeta_eth = tx_eth_wh ; rem_xbeta_eth = rem_eth_wh ; dth_xbeta_eth = dth_eth_wh}
-  if(rethnic_grp == 2){tx_xbeta_eth = tx_eth_nonwh ; rem_xbeta_eth = rem_eth_nonwh ; dth_xbeta_eth = dth_eth_nonwh}
+  if(eth == "white"){tx_xbeta_eth = tx_eth_wh ; rem_xbeta_eth = rem_eth_wh ; dth_xbeta_eth = dth_eth_wh}
+  if(eth == "nonwhite"){tx_xbeta_eth = tx_eth_nonwh ; rem_xbeta_eth = rem_eth_nonwh ; dth_xbeta_eth = dth_eth_nonwh}
   
   #Blood group #
-  if(rbg_new == 1){tx_xbeta_bld = tx_bld_o; rem_xbeta_bld = rem_bld_o; dth_xbeta_bld = dth_bld_o}
-  if(rbg_new == 2){tx_xbeta_bld = tx_bld_a; rem_xbeta_bld = rem_bld_a; dth_xbeta_bld = dth_bld_a}
-  if(rbg_new == 3){tx_xbeta_bld = tx_bld_b; rem_xbeta_bld = rem_bld_b; dth_xbeta_bld = dth_bld_b}
-  if(rbg_new == 4){tx_xbeta_bld = tx_bld_ab; rem_xbeta_bld = rem_bld_ab; dth_xbeta_bld = dth_bld_ab}
+  if(bld == "o"){tx_xbeta_bld = tx_bld_o; rem_xbeta_bld = rem_bld_o; dth_xbeta_bld = dth_bld_o}
+  if(bld == "a"){tx_xbeta_bld = tx_bld_a; rem_xbeta_bld = rem_bld_a; dth_xbeta_bld = dth_bld_a}
+  if(bld == "b"){tx_xbeta_bld = tx_bld_b; rem_xbeta_bld = rem_bld_b; dth_xbeta_bld = dth_bld_b}
+  if(bld == "ab"){tx_xbeta_bld = tx_bld_ab; rem_xbeta_bld = rem_bld_ab; dth_xbeta_bld = dth_bld_ab}
   
   
   #Matchability #
-  if(matchpts_grp == 1){tx_xbeta_mat = tx_match_1 ; rem_xbeta_mat = rem_match_1 ; dth_xbeta_mat = dth_match_1}
-  if(matchpts_grp == 2){tx_xbeta_mat = tx_match_2 ; rem_xbeta_mat = rem_match_2 ; dth_xbeta_mat = dth_match_2}
-  if(matchpts_grp == 3){tx_xbeta_mat = tx_match_3 ; rem_xbeta_mat = rem_match_3 ; dth_xbeta_mat = dth_match_3}
+  if(match == 1){tx_xbeta_mat = tx_match_1 ; rem_xbeta_mat = rem_match_1 ; dth_xbeta_mat = dth_match_1}
+  if(match == 2){tx_xbeta_mat = tx_match_2 ; rem_xbeta_mat = rem_match_2 ; dth_xbeta_mat = dth_match_2}
+  if(match == 3){tx_xbeta_mat = tx_match_3 ; rem_xbeta_mat = rem_match_3 ; dth_xbeta_mat = dth_match_3}
   
   #Dialysis #
-  if(dial_reg == 1){tx_xbeta_dial = tx_dialysis_1 ; rem_xbeta_dial = rem_dialysis_1 ; dth_xbeta_dial = dth_dialysis_1}
-  if(dial_reg == 2){tx_xbeta_dial = tx_dialysis_2 ; rem_xbeta_dial = rem_dialysis_2 ; dth_xbeta_dial = dth_dialysis_2}
+  if(dialysis == 1){tx_xbeta_dial = tx_dialysis_1 ; rem_xbeta_dial = rem_dialysis_1 ; dth_xbeta_dial = dth_dialysis_1}
+  if(dialysis == 2){tx_xbeta_dial = tx_dialysis_2 ; rem_xbeta_dial = rem_dialysis_2 ; dth_xbeta_dial = dth_dialysis_2}
   
   #Graft #
   if(graft_no_new == 0){tx_xbeta_graft = tx_graft_0 ; rem_xbeta_graft = rem_graft_0 ; dth_xbeta_graft = dth_graft_0}
   if(graft_no_new == 1){tx_xbeta_graft = tx_graft_1 ; rem_xbeta_graft = rem_graft_1 ; dth_xbeta_graft = dth_graft_1} 
   
   #Sensitisation #
-  if(hsp_reg == 1){tx_xbeta_sens = tx_sens_1 ; rem_xbeta_sens = rem_sens_1 ; dth_xbeta_sens = dth_sens_1}
-  if(hsp_reg == 2){tx_xbeta_sens = tx_sens_2 ; rem_xbeta_sens = rem_sens_2 ; dth_xbeta_sens = dth_sens_2}
+  if(sens == 1){tx_xbeta_sens = tx_sens_1 ; rem_xbeta_sens = rem_sens_1 ; dth_xbeta_sens = dth_sens_1}
+  if(sens == 2){tx_xbeta_sens = tx_sens_2 ; rem_xbeta_sens = rem_sens_2 ; dth_xbeta_sens = dth_sens_2}
 
   
   #Diabetes #

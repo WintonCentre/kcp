01c955f7072a11454cc8520ae18a6de178292847
Jack Bromley <jack@bios.health>
2024-09-09 17:17:53 +0100

    KCP-67 - version number fixes

--- validate_tag.sh
+++ validate_tag.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env sh
-TAG=$(git tag --points-at HEAD --sort=-creatordate | head -n 1)
 
-if echo "$TAG" | grep -E -q '^v-[0-9]+\.[0-9]+-[0-9]+\.[0-9]+-[0-9]+$'; then
+if [ -n "$GITHUB_HEAD_REF" ]; then
+  # we're running on github actions (detached head, it's a rebase workflow so gloss over that...)
+  HEAD_COMMIT_SHA=$(git rev-parse origin/$GITHUB_HEAD_REF)
+  TAG=$(git tag --points-at $HEAD_COMMIT_SHA --sort=-creatordate | head -n 1)
+else
+  # we're running locally
+  TAG=$(git tag --points-at HEAD --sort=-creatordate | head -n 1)
+fi
+
+if echo "$TAG" | grep -E -q '^v-[0-9]+\.[0-9]+-[0-9]+\.[0-9]+$'; then
   echo "Tag format is correct: $TAG"
 else
   echo "Tag format is incorrect: $TAG"

068990db9320cc9d81e1b2ccfdfff5abfa496e0c
Jack Bromley <phuhgh@gmail.com>
2024-09-26 10:05:12 +0100

    KCP-80 - run the tests on CI, config tests "passing" (lots is commented out...)

--- init-hooks.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-git config core.hooksPath dev/hooks
-chmod +x dev/hooks/pre-push
--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  entry: './target/index.js',
-  output: {
-    filename: 'libs.js',
-    path: path.resolve(__dirname, 'public', 'js'),
-  }
-};
